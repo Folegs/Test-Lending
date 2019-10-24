249953583b19c36ca81ba61e3c28945ccc96bf00
Folegs <folegs777@gmail.com>
2019-10-24 15:44:57 +0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('scss', mySCSS);
+
+function mySCSS(){
+    return gulp.src('./*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+}
+
+   
+  gulp.task('watch', function () {
+    gulp.watch('./*.scss', mySCSS);
+  });
